9e6216fa564b316cc66ee1fc21b46b8f3aa45a52
Burhan Yusuf <burhanyusuf@outlook.com>
2021-03-21 01:26:37 +0700

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.apiUrl || 'http://localhost:3000'
+  baseURL: process.env.REACT_APP_APIURL || 'http://localhost:3000'
 })
 
 export default instance
