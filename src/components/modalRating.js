bacd578e65830d34627e2e59062d57a932c1ea42
Burhan Yusuf <burhanyusuf@outlook.com>
2021-03-24 23:10:59 +0700

    Fix fail build

--- src/components/modalRating.js
+++ src/components/modalRating.js
@@ -91,37 +91,38 @@ function ModalRating (props) {
             <div>
               <div className='mt-3 text-center sm:mt-5'>
                 <div className='mt-2 flex px-2'>
-                <div class='justify-self-center'>
-                <h3
-                  className='text-lg leading-6 mb-4 font-medium text-gray-900'
-                  id='modal-rating'
-                >
-                  Rating
-                </h3>
-                <div class='flex justify-center'>
-                  <ReactStars
-                    count={5}
-                    onChange={ratingChanged}
-                    size={24}
-                    color2={'#ffd700'}
-                    value={formValue.rating}
-                    class='inline-flex'
-                  />
-                </div>
-              </div>
-              <br />
-              <div class=''>
-                <h3
-                  className='text-lg leading-6 mb-4 font-medium text-gray-900'
-                  id='modal-headline'
-                >
-                  Review
-                </h3>
-                <textarea
-                  placeholder='Review'
-                  name='review'
-                  id='review'
-                  rows='5'
+                  <div class='sm:col-span-12 w-full'>
+                    <div class='justify-self-center'>
+                      <h3
+                        className='text-lg leading-6 mb-4 font-medium text-gray-900'
+                        id='modal-rating'
+                      >
+                        Rating
+                      </h3>
+                      <div class='flex justify-center'>
+                        <ReactStars
+                          count={5}
+                          onChange={ratingChanged}
+                          size={24}
+                          color2={'#ffd700'}
+                          value={formValue.rating}
+                          class='inline-flex'
+                        />
+                      </div>
+                    </div>
+                    <br />
+                    <div class=''>
+                      <h3
+                        className='text-lg leading-6 mb-4 font-medium text-gray-900'
+                        id='modal-headline'
+                      >
+                        Review
+                      </h3>
+                      <textarea
+                        placeholder='Review'
+                        name='review'
+                        id='review'
+                        rows='5'
                         value={formValue.review}
                         onChange={reviewChanged}
                         class='py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
@@ -129,8 +130,7 @@ function ModalRating (props) {
                     </div>
                     <button
                       type='button'
-                      class='mt-2 inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                      onClick={submitReview}
+                      class='mt-2 inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' onClick={submitReview}
                     >
                       Add review
                     </button>
